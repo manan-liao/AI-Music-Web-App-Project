15e27e80380ca09c74c4beccb786474268b6a576
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-10-26 18:11:16 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song1 = "";
 song2 = "";
 
+leftWristx = 0;
+leftWristy = 0;
+rightWristx = 0;
+rightWristy = 0;
+
 function preload(){
     song1 = loadSound("music1.mp3");
     song2 = loadSound("music2.mp3");
@@ -11,8 +16,24 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,500);
+}
+
+function gotPoses(){
+    if(results.length>0){
+        console.log(results);
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.y;
+    }
+}
+
+function modelLoaded(){
+    console.log("posenet is initialized");
 }
